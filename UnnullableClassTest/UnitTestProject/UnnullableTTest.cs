0f1d3452961d204f72cda34429ae3dbdbf7a0402
LAVN <rairairakurakulilac@hotmail.co.jp>
2015-04-15 23:24:14 +0900

    Unnullable<T>クラスをチェックイン

---
+++ UnnullableClassTest/UnitTestProject/UnnullableTTest.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UnnulableClassTest;
+
+namespace UnitTestProject
+{
+	[TestClass]
+	public class UnnullableTTest
+	{
+		[TestMethod]
+		public void NullInitalizeTest()
+		{
+			try
+			{
+				Unnullable<object> b = new Unnullable<object>(null);
+				Assert.Fail("null initialize successed.");
+			}
+			catch (NullReferenceException)
+			{
+				// Success
+			}
+			catch (Exception ex)
+			{
+				Assert.Fail(ex.Message);
+			}
+		}
+
+		[TestMethod]
+		public void UnNullInitializeTest()
+		{
+			try
+			{
+				Unnullable<int> a = new Unnullable<int>(1);
+				// Success.
+			}
+			catch (Exception ex)
+			{
+				Assert.Fail(ex.Message);
+			}
+		}
+
+		[TestMethod]
+		public void CompareTest()
+		{
+			Unnullable<int> a = new Unnullable<int>(1);
+			int c = a.Value;
+
+			if (c == a)
+			{
+				// Success.
+			}
+			else
+			{
+				Assert.Fail("Compare failed.");
+			}
+		}
+	}
+}
---
+++ UnnullableClassTest/UnnullableClassTest/Unnullable{T}.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Runtime;
+
+namespace UnnulableClassTest
+{
+	[Serializable]
+	public class Unnullable<T>
+	{
+		private bool hasValue;
+		internal T value;
+
+#if !FEATURE_CORECLR
+		[TargetedPatchingOptOut("Performance critical to inline across NGen image boundaries")]
+#endif
+		public Unnullable(T value)
+		{
+			if (value == null)
+			{
+				throw new NullReferenceException("Null Initialized Unnullable<T>");
+			}
+
+			this.value = value;
+			this.hasValue = true;
+		}
+
+		public bool HasValue
+		{
+			get
+			{
+				return hasValue;
+			}
+		}
+
+		public T Value
+		{
+#if !FEATURE_CORECLR
+			[TargetedPatchingOptOut("Performance critical to inline across NGen image boundaries")]
+#endif
+			get
+			{
+				if (!HasValue)
+				{
+					//ThrowHelper.ThrowInvalidOperationException(ExceptionResource.InvalidOperation_NoValue);
+				}
+				return value;
+			}
+		}
+
+#if !FEATURE_CORECLR
+		[TargetedPatchingOptOut("Performance critical to inline across NGen image boundaries")]
+#endif
+		public T GetValueOrDefault()
+		{
+			return value;
+		}
+
+		public T GetValueOrDefault(T defaultValue)
+		{
+			return HasValue ? value : defaultValue;
+		}
+
+		public override bool Equals(object other)
+		{
+			if (!HasValue) return other == null;
+			if (other == null) return false;
+			return value.Equals(other);
+		}
+
+		public override int GetHashCode()
+		{
+			return HasValue ? value.GetHashCode() : 0;
+		}
+
+		public override string ToString()
+		{
+			return HasValue ? value.ToString() : "";
+		}
+
+		public static implicit operator T(Unnullable<T> value)
+		{
+			return value.Value;
+		}
+
+		public static explicit operator Unnullable<T>(T value)
+		{
+			return new Unnullable<T>(value);
+		}
+	}
+}
